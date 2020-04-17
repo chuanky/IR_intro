fe601cf03421c11b0157948af287bfc3348f8204
chuanky <shicq01@gmail.com>
2020-04-17 13:20:50 +0800

    Init add TODO for Chapter 1

---
+++ Chapter1.cpp
@@ -0,0 +1,27 @@
+/*
+	Boolean Retrieval Model
+
+	TODO:
+	input: collection{docID: content}
+	output: 
+	1. term-document incidence matrix
+	2. invert index
+*/
+
+class BooleanRetrievalModel
+{
+public:
+	BooleanRetrievalModel();
+	~BooleanRetrievalModel();
+
+private:
+
+};
+
+BooleanRetrievalModel::BooleanRetrievalModel()
+{
+}
+
+BooleanRetrievalModel::~BooleanRetrievalModel()
+{
+}
---
+++ IR_intro.cpp
@@ -0,0 +1,8 @@
+// This project aims to solve the exercies of <An Introduction for Information Retrieval>
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
